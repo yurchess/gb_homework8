bec7bc580e917c2684021afb9213048650285408
Yury Mitroshin <yurchess@mail.ru>
2017-12-18 13:44:20 +0400

    Second commit

--- Window.java
+++ Window.java
@@ -1,15 +1,39 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.io.File;
 
 public class Window extends JFrame {
+
     public static void main(String[] args) {
         Window window = new Window();
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLocation(100,200);
         window.setSize(300,400);
-        window.setVisible(true);
 
-        JButton button = new JButton("MyBtn");
+        JButton button = new JButton("Open image");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JFileChooser fileChooser = new JFileChooser();
+                int retunrVal = fileChooser.showOpenDialog(window);
+                if (retunrVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                }
+            }
+        });
         window.getContentPane().add(BorderLayout.NORTH, button);
+
+        window.setVisible(true);
     }
+
+    class Panel extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+        }
 }
